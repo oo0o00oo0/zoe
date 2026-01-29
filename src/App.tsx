a4cb85784812a7c6d9ff4fd58e6e1a588d3d11e5
oo0o00oo0 <dsgnrm@gmail.com>
2026-01-29 17:20:08 +0000

    web

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ function App() {
   return (
     <div className="app">
       <h1>🫶</h1>
+      <h2>
+        <a href="https://www.instagram.com/reel/DTiYxZdjklL/?igsh=NzVzM256M3Nkb2Rh">
+          Stella Webb
+        </a>
+      </h2>
     </div>
   );
 }
